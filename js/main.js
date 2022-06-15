88d7476f120038edcca8c6e7baa2aa186bb4898d
KhodorShhoury <khodorshhoury1@gmail.com>
2022-06-15 20:31:06 +0300

    add files

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
+//header variables
 let triggerButton = document.querySelector(".menu-trigger");
-let navLinks = document.querySelector(".nav-links");
+let navBar = document.querySelector(".nav-links");
 let menuTrigger = [...document.querySelectorAll(".menu-trigger span")]
+let navBarLinks = [...navBar.querySelectorAll(".link")]
+let headerSection = document.querySelector(".header")
+let mainSection = document.querySelector(".main");
+let bodyDirectElements = [...document.body.children];
+//functions
 
+//trigger navbar function
 function triggerNavBar(){
-    if(navLinks.classList.contains("active")){
-        navLinks.classList.remove("active")
+    if(navBar.classList.contains("active")){
+        navBar.classList.remove("active")
         menuTrigger.forEach((ele => ele.classList.remove("active")))
     }else{
-        navLinks.classList.add("active");
+        navBar.classList.add("active");
         menuTrigger.forEach((ele => ele.classList.add("active")))
     }
 }
+
+function toggleNavBarLinks(){
+    navBar.classList.remove("active")
+    menuTrigger.forEach((ele => ele.classList.remove("active")))
+
+
+    navBarLinks.forEach(link => link.classList.remove("active"));
+    this.classList.add("active")
+}
+
+navBarLinks.forEach(link=>link.addEventListener("click",toggleNavBarLinks))
+
+//trigger navbar
 triggerButton.addEventListener("click",triggerNavBar);
 
-let headerSection = document.querySelector(".header")
-let mainSection = document.querySelector(".main");
-
-window.onscroll = function(){
+function toggleHeader(){
     if(document.documentElement.scrollTop >=  mainSection.offsetHeight - 80){
         headerSection.classList.add("fixed");
     }else{
         headerSection.classList.remove("fixed")
     }
 }
+
+window.onscroll = function(){
+   
+    toggleHeader();
+    
+}
+
+
